d5d182cd34a9a5ce2e7328fb6bf2e704b93dc004
Ricardo Castro <ricardormcastro@gmail.com>
2020-07-16 22:51:59 -0300

    first commit

---
+++ src/br/com/caelum/jdbc/ConnectionFactory.java
@@ -0,0 +1,17 @@
+package br.com.caelum.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	public Connection getConnection() {
+		try {
+			return DriverManager.getConnection(
+					"jdbc:mysql://127.0.0.1/fj21", "root", "");
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
